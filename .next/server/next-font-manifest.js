f2236d48f95adc90e1051d17285c92b45881c30b
Vinod C <viinodc@gmail.com>
2025-07-29 20:19:45 +0530

    Remove .next folder and react table for users

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/prasanth/projects/landau-dashboard/frontend/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
